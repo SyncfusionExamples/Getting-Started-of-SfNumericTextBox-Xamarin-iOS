d58dcbbc70a70e2af1de237dc7b7dc0f69c20a63
Anand Raj <anandraj.selvam@syncfusion.com>
2020-06-11 16:50:36 +0530

    Sample included

---
+++ NumericTextBox_Sample/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using CoreGraphics;
+using Foundation;
+using Syncfusion.SfNumericTextBox.iOS;
+using System;
+using UIKit;
+
+namespace NumericTextBox_Sample
+{
+    public partial class ViewController : UIViewController
+    {
+        SfNumericTextBox numericTextBox;
+        public ViewController(IntPtr handle) : base(handle)
+        {
+            numericTextBox = new SfNumericTextBox()
+            {
+                Frame = new CGRect(10, 50, 350, 40),
+                Value = 1000,
+                Watermark = "Principal Amount",
+                MaximumNumberDecimalDigits = 2,
+                FormatString = "c",
+                AllowNull = true,
+                CultureInfo = new NSLocale("en_us"),
+                PercentDisplayMode = SFNumericTextBoxPercentDisplayMode.Compute,
+            };
+
+            this.Add(numericTextBox);
+        }
+
+      
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
